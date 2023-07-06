80210d5af473f410a7fb464f9dad7fc92f68c3b0
hr_kang <hyerim513@gmail.com>
2023-07-06 11:21:41 +0900

    [refactoring] add TestFixture

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,12 +1,16 @@
 #include "pch.h"
 #include "../SimilarityChecker/SimilarityChecker.cpp"
 
-TEST(TestCaseName, SameLength) {
+class SimilarityCheckerFixture : public testing::Test
+{
+public:
 	SimilarityChecker checker;
+};
+
+TEST_F(SimilarityCheckerFixture, SameLength) {
 	EXPECT_EQ(60, checker.getLengthPoint("ASD", "DSA"));
 }
 
-TEST(TestCaseName, DoubleLength) {
-	SimilarityChecker checker;
+TEST_F(SimilarityCheckerFixture, DoubleLength) {
 	EXPECT_EQ(0, checker.getLengthPoint("A", "BB"));
 }
