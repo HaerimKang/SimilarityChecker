f10ce587f1522ac7d312a34d93ac892f31406b91
hr_kang <hyerim513@gmail.com>
2023-07-06 13:33:14 +0900

    [refactoring] update SimilarityChecker

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,32 +1,42 @@
 #include <string>
-
+#include <vector>
 using namespace std;
 
-class SimilarityChecker
-{
+class SimilarityChecker {
 public:
-	int getPartialPoint(string text1, string text2)
-	{
-		int gap = abs((int)text1.size() - (int)text2.size());
+	int getLengthPoint(const string& str1, const std::string& str2) {
+		int A = getLongLength(str1, str2);
+		int B = getShortLength(str1, str2);
 
-		int shortTextSize = text1.size();
-		if (text1.size() > text2.size())
-		{
-			shortTextSize = text2.size();
-		}
-		return (1 - ( (double) gap / shortTextSize)) * 60;
+		if (isSameLength(A, B)) return MAX_LENGTH_POINT;
+		if (isDoubleLengthGap(A, B)) return MIN_LENGTH_POINT;
+		return getPartialLengthPoint(A, B);
 	}
 
-	int getLengthPoint(string text1, string text2)
-	{
-		if (text1.size() == text2.size()) return 60;
-		else if (text1.size() >= text2.size() * 2
-			|| text1.size() * 2 <= text2.size())
-		{
-			return 0;
-		}
-		else {
-			return getPartialPoint(text1, text2);
-		}
+private:
+	int getLongLength(const string& str1, const string& str2) {
+		if (str1.length() > str2.length()) return str1.length();
+		return str2.length();
 	}
+
+	int getShortLength(const string& str1, const string& str2) {
+		if (str1.length() < str2.length()) return str1.length();
+		return str2.length();
+	}
+
+	bool isSameLength(int A, int B) {
+		return A == B;
+	}
+
+	bool isDoubleLengthGap(int A, int B) {
+		return A >= B * 2;
+	}
+
+	int getPartialLengthPoint(int A, int B) {
+		int GAP = A - B;
+		return (B - GAP) * MAX_LENGTH_POINT / B;
+	}
+
+	const int MAX_LENGTH_POINT = 60;
+	const int MIN_LENGTH_POINT = 0;
 };
